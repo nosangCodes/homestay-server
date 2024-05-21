16c60f7046aca08a24ff0096fd330f823b70f5c5
Nosang Subba <nosangdev@gmail.com>
2024-05-21 15:53:48 +0530

    handle cors error

--- src/server.ts
+++ src/server.ts
@@ -10,10 +10,24 @@ import cookieParser from "cookie-parser";
 export const createServer = (): Express => {
   const app = express();
 
+  const allowedOrigins = [
+    "http://localhost:5173",
+    "https://homestay-admin-xi.vercel.app",
+    "https://homestay-admin.nosang.in",
+  ];
+
   app
     .use(
       cors({
-        origin: "http://localhost:5173",
+        origin: function (origin, callback) {
+          // Allow requests with no origin (like mobile apps or curl requests)
+          if (!origin) return callback(null, true);
+          if (allowedOrigins.indexOf(origin) !== -1) {
+            callback(null, true);
+          } else {
+            callback(new Error("Not allowed by CORS"));
+          }
+        },
         credentials: true,
       })
     )
